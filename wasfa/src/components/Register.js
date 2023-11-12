096b8ec3e2442347d529aa77684e36f83654219b
FaiNasserAlkahtani <mariamkalenizi@gmail.com>
2023-11-12 20:09:54 +0300

    register format

--- wasfa/src/components/Register.js
+++ wasfa/src/components/Register.js
@@ -1,12 +1,3 @@
-// import React from "react";
-
-// const Register = () => {
-//   return <div> HI</div>;
-// };
-
-// export default Register;
-
-//
 import { useMutation } from "@tanstack/react-query";
 import React, { useContext, useState } from "react";
 import { register } from "../api/auth";
@@ -43,12 +34,15 @@ const Register = () => {
       <div>
         <div className="max-w-1/3 ">
           <div className="flex mx-auto gap-2 "></div>
-          <div className="font-sans text-2xl font-black text-center my-8">
-            Welcome to your Bank
+          <div className="font-sans text-4xl font-black text-center my-8">
+            Create an account
           </div>
-          <h1 className="font-sans text-4xl font-black text-center mt-4">
-            Register
+          <h1 className="font-sans font-black text-center">
+            Sign up to save and review your favorite recipes.{" "}
           </h1>
+          {/* <h1 className="font-sans text-2xl font-black text-center mt-4">
+            Register
+          </h1> */}
           <form onSubmit={handleForm} className="mt-8 mx-auto">
             <label htmlFor="name" className="my-4 flex content-center gap-4">
               <span className="w-1/3 self-center text-sm font-medium">
@@ -79,7 +73,8 @@ const Register = () => {
                 required
               ></input>
             </label>
-            <label htmlFor="image" className="my-4 flex content-center gap-4">
+
+            {/* <label htmlFor="image" className="my-4 flex content-center gap-4">
               <span className="w-1/3 self-center text-sm font-medium">
                 Profile Image
               </span>
@@ -91,7 +86,7 @@ const Register = () => {
                 className=" w-2/3 block text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
                 required
               ></input>
-            </label>
+            </label> */}
             <button
               type="submit"
               className="bg-yellow-500 py-2 px-4 rounded-lg hover:bg-white hover:text-yellow-500 w-full my-8"
